4233896559503658f16430c072c674f23eef5704
Brook Gebrechristos <brookzer@gmail.com>
2016-02-23 14:43:01 -0800

    Add front-end form to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,26 @@
 var socket = io();
+//io variable is defined when we load the socket io library in html
+//this connects the server to the backend 
 
 socket.on('connect', function() {
   console.log("Connected to socket.io server");
 });
 
 socket.on('message', function(message) {
-  //data from the message we created on server.js
+  //callback function gets data from the message we created on server.js
   console.log("New Message:\n" + message.text);
+});
+
+//Handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event) {
+  event.preventDefault();
+  //handle the form submission on our own when using socket and ajax request
+  var $message = $form.find('input[name=message]');
+  socket.emit('message', {
+    text: $message.val()
+  });
+  
+  $message.val('');
 });
--- server.js
+++ server.js
@@ -1,28 +1,25 @@
-var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-//anything express app listens to the server should also listens to
 
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket) {
-  console.log('User connected via socket.io!');
+  console.log('User connected to the client!');
   
   socket.on('message', function(message) {
     console.log("Message received: " + message.text);
-    
+
     socket.broadcast.emit('message', message);
-    //sends it to everybody but the person who sent it
-    //if you want to send it including the perosn who send it, then use io.emit
   });
+  
   socket.emit('message', {
     text :'Welcome to the chat application'
-  })
+  });
 });
-//1st is arg is an event, for this example, on the connection event
 
 http.listen(PORT, function() {
   console.log("Server started!");
